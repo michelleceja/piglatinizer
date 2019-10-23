30480ede0151e35e5725646894655d0f45fb2f79
michelleceja <michelle.cr@aspirestudent.org>
2019-10-23 01:23:26 +0000

    comment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-$( "document" ).ready(function() {
+$("document").ready(function() {
 
-$("#button").click(function() {
-    let password = $("#buttonInput").val();
-    $(.output)
-}
+    $("#button").click(function() {
+        var userInput = $(".input").val();
+        var pigLatinWord = pigLatinizer(userInput);
+        $(".output").text(pigLatinWord)
+    });
 
 });
+
+function pigLatinizer(userInput) {
+  var vowels =["a","e","i","o","u"]
+  var firstLetter =userInput[0];
+  if (vowels.includes(firstLetter)) {
+    return userInput + "ay"
+  } else {
+      userInput.slice(1)+firstLetter + "ay"
+  }
+
+
+}
